c3b0be9057d3996e213cf6b44f862f6ca5b5fe93
Jakub Stasiak <jakub@stasiak.at>
2021-03-15 21:57:36 +0100

    Fix some links, they're 404-ing now

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 //! Provides a mockable reqwest-like HTTP client.
 //!
-//! Write your code generic over the [Client](trait.Client.html) trait,
-//! and in production use [DirectClient](struct.DirectClient.html) while in testing
-//! you can use [ReplayClient](struct.ReplayClient.html), which will record a request
+//! Write your code generic over the [Client](client/trait.Client.html) trait,
+//! and in production use [DirectClient](client/struct.DirectClient.html) while in testing
+//! you can use [ReplayClient](client/struct.ReplayClient.html), which will record a request
 //! the first time and replay it every time the exact same request is made in the
 //! future.
 //!
